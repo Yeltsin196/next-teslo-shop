9a50f7b0a9468d70e061d368491a9f25eb4972bc
Yeltsin <yramirez@shokworks.io>
2025-03-02 20:53:51 -0400

    Add initial shop layout, pages, and UI components

---
+++ src/app/(shop)/category/not-found.tsx
@@ -0,0 +1,8 @@
+import { PageNotFound } from '@/components';
+import React from 'react'
+
+const notfound = () => {
+	return <PageNotFound />;
+}
+
+export default notfound
---
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+	return (
+		<div>Producto</div>
+	)
+}
+
+export default page
