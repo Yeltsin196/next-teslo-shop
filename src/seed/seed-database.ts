1115650c5e6c83e173495232e027fee1d10924b4
Yeltsin <yramirez@shokworks.io>
2025-05-12 14:31:30 -0400

    feat(address): implement user address management with CRUD operations and country retrieval

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,11 +1,13 @@
-
-import { initialData } from "./seed";
 import prisma from "../lib/prisma";
+import { initialData } from "./seed";
+import { countries } from "./seed-countries";
 
 async function main() {
   // 1. Borrar registros previos
   // await Promise.all( [
+  await prisma.userAddress.deleteMany();
   await prisma.user.deleteMany();
+  await prisma.country.deleteMany();
 
   await prisma.productImage.deleteMany();
   await prisma.product.deleteMany();
@@ -16,6 +18,10 @@ async function main() {
 
   await prisma.user.createMany({
     data: users,
+  });
+
+  await prisma.country.createMany({
+    data: countries,
   });
 
   //  Categorias
