15b47f590bbc4f846d0a3b27c50040e95c2df855
Yeltsin <yramirez@shokworks.io>
2025-05-12 20:43:38 -0400

    feat(payments): integrate PayPal payment processing with transaction handling and order status updates

--- src/app/(shop)/orders/[id]/page.tsx
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -1,19 +1,9 @@
-/* import Link from "next/link"; */
-
-import { Title } from "@/components";
-/* import { initialData } from "@/seed/seed"; */
-import Image from "next/image";
-import clsx from "clsx";
-import { IoCardOutline } from "react-icons/io5";
-import { getOrderById } from "@/actions/order/get-order-by-id";
 import { redirect } from "next/navigation";
-import { currencyFormat } from "@/utils";
+import Image from "next/image";
 
-/* const productsInCart = [
-  initialData.products[0],
-  initialData.products[1],
-  initialData.products[2],
-]; */
+import { getOrderById } from "@/actions/order/get-order-by-id";
+import { currencyFormat } from "@/utils";
+import { OrderStatus, PayPalButton, Title } from "@/components";
 
 interface Props {
   params: {
@@ -42,21 +32,7 @@ export default async function OrdersByIdPage({ params }: Props) {
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
           {/* Carrito */}
           <div className="flex flex-col mt-5">
-            <div
-              className={clsx(
-                "flex items-center rounded-lg py-2 px-3.5 text-xs font-bold text-white mb-5",
-                {
-                  "bg-red-500": !order!.isPaid,
-                  "bg-green-700": order!.isPaid,
-                }
-              )}
-            >
-              <IoCardOutline size={30} />
-              {/* <span className="mx-2">Pendiente de pago</span> */}
-              <span className="mx-2">
-                {order?.isPaid ? "Pagada" : "No pagada"}
-              </span>
-            </div>
+            <OrderStatus isPaid={order?.isPaid ?? false} />
 
             {/* Items */}
             {order!.OrderItem.map((item) => (
@@ -133,21 +109,11 @@ export default async function OrdersByIdPage({ params }: Props) {
             </div>
 
             <div className="mt-5 mb-2 w-full">
-              <div
-                className={clsx(
-                  "flex items-center rounded-lg py-2 px-3.5 text-xs font-bold text-white mb-5",
-                  {
-                    "bg-red-500": !order!.isPaid,
-                    "bg-green-700": order!.isPaid,
-                  }
-                )}
-              >
-                <IoCardOutline size={30} />
-                {/* <span className="mx-2">Pendiente de pago</span> */}
-                <span className="mx-2">
-                  {order?.isPaid ? "Pagada" : "No pagada"}
-                </span>
-              </div>
+              {order?.isPaid ? (
+                <OrderStatus isPaid={order?.isPaid ?? false} />
+              ) : (
+                <PayPalButton amount={order!.total} orderId={order!.id} />
+              )}
             </div>
           </div>
         </div>
